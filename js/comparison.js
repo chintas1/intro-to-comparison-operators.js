9facf8f06620290521edc080b5c357cfe3c37ae9
chintas1 <chintas1@tcnj.edu>
2016-03-21 15:23:57 -0400

    Done.

--- js/comparison.js
+++ js/comparison.js
@@ -1,1 +1,26 @@
+function testFalseLooseEqualityComparison(){
+  var num1 = 0;
+  var num2 = 1;
+  return(num1 == num2);
+}
+function testTrueLooseNonEqualityComparison(){
+  var num1 = 0;
+  var num2 = 1;
+  return(num1 != num2);
+}
+function testTrueLooseEqualityComparison(){
+  var num1 = 1;
+  var num2 = "1";
+  return(num1 == num2);
+}
+function testFalseStrictEqualityComparison(){
+  var num1 = 1;
+  var num2 = "1";
+  return(num1 === num2);
+}
+function testTrueStrictEqualityComparison(){
+  var num1 = 1;
+  var num2 = "1";
+  return(num1 === parseInt(num2));
+}
 'use strict';
